b886fa10cdba92928145bbd85404f0fed56ef3ab
Yizzy Wu <wut343@newschool.edu>
2017-01-21 03:09:55 -0500

    second update with project pages and rellax.js scroll effects

--- scripts/scroll_speeds.js
+++ scripts/scroll_speeds.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+// =================================================================================
+// scroll speed
+// =================================================================================
 	var scrollItem = $('.scroll-speeds');
 	$(window).scroll(function(){
 		var scrollTop = $(window).scrollTop();
@@ -11,17 +14,21 @@ $(document).ready(function(){
 		})
 	})
 
-	//click arrow to about div
+// =================================================================================
+// click arrow to about div
+// =================================================================================
 	$('#scroll-down-arrow').click(function(){
     	$('body,html').animate({
-        	scrollTop: $("#about-me-title").offset().top - 300
+        	scrollTop: $("#about-me-title").offset().top - 280
     	}, 500);
 	})
 
-
+// =================================================================================
+// project list click scroll to divs
+// =================================================================================
 	$('.project-li').click(function(){
 		var scrollAnchor = $(this).attr('data-scroll'),
-            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
+            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 100;
 
     	$('body,html').animate({
         	scrollTop: scrollPoint
@@ -29,4 +36,25 @@ $(document).ready(function(){
 
     	return false;
 	})
+
+// =================================================================================
+// scroll top and button fixed
+// =================================================================================
+	$(window).scroll(function(){
+		var windowscroll = $(window).scrollTop();
+		if(windowscroll >= 300) {
+			$('#scroll-top-button').removeClass('absolute');
+			$('#scroll-top-button').addClass('fixed');
+		} else {
+			$('#scroll-top-button').removeClass('fixed');
+			$('#scroll-top-button').addClass('absolute');
+		}
+	})
+	$('#scroll-top-button').click(function(){
+		$('body,html').animate({
+        	scrollTop: 0,
+        	smooth: true
+    	}, 800);
+	})
+
 })
